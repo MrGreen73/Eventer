9bddf79e3a1bb621cfdd14087e490b31aeadd5bf
Ivan <damag666@gmail.com>
2018-04-20 17:53:06 +0300

    Добавлен Firebase Сделана регистрация пользователя

--- android_app/app/src/main/java/com/ivan/eventer/controller/StartActivity.java
+++ android_app/app/src/main/java/com/ivan/eventer/controller/StartActivity.java
@@ -5,19 +5,34 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.ivan.eventer.R;
 import com.ivan.eventer.view.StartFragment;
 
 public class StartActivity extends AppCompatActivity {
+
+    // Firebase
+    private FirebaseAuth mFirebaseAuth; // Для проверки аутентификации
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mFirebaseAuth.getCurrentUser();
 
         // Необходима проверка: авторизован ли пользователь
-        if (false) {
+        if (currentUser == null) {
 
             sentToStart();//Если не авторизован
 
@@ -26,8 +41,8 @@ public class StartActivity extends AppCompatActivity {
             sentToMain();//Если авторизован
 
         }
-    }
 
+    }
 
     //Посылает пользователя на главную страницу
     private void sentToMain() {
--- android_app/app/src/main/java/com/ivan/eventer/view/RegisterFragment.java
+++ android_app/app/src/main/java/com/ivan/eventer/view/RegisterFragment.java
@@ -2,16 +2,25 @@ package com.ivan.eventer.view;
 
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 import com.ivan.eventer.R;
 import com.ivan.eventer.controller.MainActivity;
 
@@ -28,6 +37,10 @@ public class RegisterFragment extends Fragment {
     private Button mButton;
     private ProgressDialog mProgressDialog;
 
+    //Firebase
+    private FirebaseAuth mFirebaseAuth;
+    private DatabaseReference mDatabaseReference;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -42,6 +55,8 @@ public class RegisterFragment extends Fragment {
         mButton = v.findViewById(R.id.regBtn);
         mProgressDialog = new ProgressDialog(getActivity());
 
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
         mButton.setOnClickListener(v1 -> {
 
             String name = mName.getText().toString();
@@ -50,6 +65,8 @@ public class RegisterFragment extends Fragment {
             String city= mCity.getText().toString();
             String password = mPassword.getText().toString();
 
+            View focusView = null;
+
             if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(age) && !TextUtils.isEmpty(city) && !TextUtils.isEmpty(password)) {
 
                 mProgressDialog.setTitle(getString(R.string.progressDialogRegister));
@@ -57,12 +74,49 @@ public class RegisterFragment extends Fragment {
                 mProgressDialog.setCanceledOnTouchOutside(false);
                 mProgressDialog.show();
 
+                InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
                 registerUser(name, email, age, city, password);
 
 
             } else {
 
-                //TODO: добавить shackbar здесь
+                if (TextUtils.isEmpty(password)){
+
+                    mPassword.setError("Введите пароль");
+                    focusView = mPassword;
+
+                }
+
+                if (TextUtils.isEmpty(city)) {
+
+                    mCity.setError("Введите город");
+                    focusView = mCity;
+
+                }
+
+                if (TextUtils.isEmpty(age)) {
+
+                    mAge.setError("Введите возраст");
+                    focusView = mAge;
+
+                }
+
+                if (TextUtils.isEmpty(email)) {
+
+                    mEmail.setError("Введите почту");
+                    focusView = mEmail;
+
+                }
+
+                if (TextUtils.isEmpty(name)) {
+
+                    mName.setError("Введите имя");
+                    focusView = mName;
+
+                }
+                focusView.requestFocus();
 
             }
 
@@ -74,13 +128,31 @@ public class RegisterFragment extends Fragment {
 
     private void registerUser(String name, String email, String age, String city, String password) {
 
-        //TODO: добавить регистрацию пользователя
+        mFirebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
 
-        mProgressDialog.dismiss();
-        sentToMain();
+            if (task.isSuccessful()){
+
+
+                mProgressDialog.dismiss();
+                sentToMain();
+
+            } else {
+
+                mProgressDialog.hide();
+                Snackbar snackbar = Snackbar.make(getView(), "Не получилось зарегистрировать пользователя", Snackbar.LENGTH_LONG);
+                snackbar.setAction("Повторить", v1 -> {
+
+                   registerUser(name, email, age, city, password);
+
+                });
+
+                snackbar.show();
+
+            }
+
+        });
 
     }
-
 
     //Посылает пользователя на главную страницу
     private void sentToMain() {
