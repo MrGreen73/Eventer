c60217b31a000c6202c7b24888a192eecaf1cca5
Ivan <damag666@gmail.com>
2018-05-05 16:07:44 +0300

    Добавил новые файлы

---
+++ types/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.List;
+
+
+public class Main {
+    {
+        System.out.println("Kek");
+    }
+
+
+    public static void main(String[] args) {
+
+        int x = 0;
+        System.out.print(++x==x++);
+
+        HashMap<String, Character> a;
+
+    }
+
+}
+
